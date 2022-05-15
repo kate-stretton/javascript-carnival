a1e6f605fa3d9a4b7d1ee43a486208789939b226
Kate Stretton <katejeanstretton@gmail.com>
2022-05-15 15:33:23 +1200

    add html file

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,47 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+
+// Create an array of unicorn images
+let unicorns = document.getElementsByTagName('img')
+
+// Many failed attempts at giving the unicorns names before adding name as an ID to CSS file
+// let Kate = document.getElementById('unicorn1')
+// let Justine = document.getElementById('unicorn2')
+// let Emily = document.getElementById('unicorn3')
+
+// unicorns[0] = Kate
+// unicorns[1] = Justine
+// unicorns[2] = Emily
+
+// Write a function to change images when clicking on unicorns
+function unicornClicked(e) {
+  // Variable to click on specific unicorn
+  let unicorn = e.target
+  // If unicorn image is 0, change to 1
+  if (unicorn.src.match('./images/unicorn-0.png')) {
+    unicorn.src = './images/unicorn-1.png'
+    // If unicorn image is 1, change to 2
+  } else if (unicorn.src.match('./images/unicorn-1.png')) {
+    unicorn.src = './images/unicorn-2.png'
+    // If unicorn image is 2, change to 3
+  } else if (unicorn.src.match('./images/unicorn-2.png')) {
+    unicorn.src = './images/unicorn-3.png'
+    // Alert player that unicorn is grateful when image is 3!
+    alert(unicorn.id + ' says thanks!')
+  }
+}
+
+// Loop through array of unicorn images
+for (let i = 0; i < unicorns.length; i++) {
+  unicorns[i].onclick = unicornClicked
+}
+
+// This was a previous ttempt at grateful unicorns before I realised the alert had to happen within unicornClicked
+// function unicornGratitude() {
+//   if ((unicorns.src = './images/unicorn-3.png')) {
+//     alert(unicorns.id + ' says thanks!')
+//   }
+// }
--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,35 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+// References to the cells
+let moleHill = document.getElementsByTagName('td') // All the cells
+let mole = document.createElement('img') // An element for the mole image
+let audio = new Audio('whack-audio.wav')
+
+// Function to select a random cell and have the mole image appear
+function showMole() {
+  // Randomly generate a number between 1 & 25
+  let randomNumber = Math.floor(Math.random() * moleHill.length)
+  // Apply random number to a cell
+  let randomCell = moleHill[randomNumber]
+  // Define mole image
+  mole.src = 'mole.png'
+  // Add mole image to the cell
+  randomCell.appendChild(mole)
+}
+
+// Call function for mole to appear in random cell when browser loads
+showMole()
+
+// Function for clicking on mole
+mole.onclick = function whackedMole() {
+  // Remove mole from current cell
+  mole.remove()
+  // Play audio sound
+  audio.play()
+  // Reveal mole in new cell
+  showMole()
+}
